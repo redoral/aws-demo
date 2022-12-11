4cbfc11d708aeafb26096949a931775195ce0221
Red Oral <redmunozoral@gmail.com>
2022-12-10 21:03:34 -0800

    Initial commit. Includes documentation and lambda code.

---
+++ README.md
@@ -0,0 +1,100 @@
+# AWS Demo
+
+Simple documentation for the AWS Serverless API that I use to demo for interviews. Includes the lambda code, see `lambda.js`. For usage and response formats, see below.
+
+### Services
+
+- AWS Lambda
+- AWS API Gateway
+- AWS DynamoDB
+
+## Usage
+
+The API is a simple RESTful products API with complete CRUD. The available endpoints and methods are:
+
+- `GET` on `/products` will return all of the entries in the products table.
+- `POST` on `/products` will insert a new product into the products table; requires the following body:
+
+```json
+{
+    "productId": string, // ex: "0"
+    "productName": string, // ex: "Shirt"
+    "productCategory": string, // ex: "Clothing"
+    "productPrice": string // ex: "5.99"
+}
+```
+
+DynamoDB takes values in strings, that's why everything is a string. The types are defined after this request body is received and before it is put in the table.
+
+- `GET` on `/products/{productId}` returns a product that matches the given ID, where **{productId}** is a numerical value.
+- `PATCH` on `/products/{productId}` updates an existing product in the products table based on the given ID in the URI and with the given values in the body; requires the following body:
+
+```json
+{
+  "updateKey": string, // ex: "productPrice"
+  "updateValue": {type: string} // ex: {"N": "59.99"}
+}
+```
+
+- `DELETE` on `/products/{productId}` deletes an existing product in the table based on the given ID in the URI.
+- `GET` on `/products/categories` returns all available categories.
+- `GET` on `/products/categories/{categoryName}` returns all available products under the specified category name.
+
+## Successful Responses
+
+The response format and status codes depends on the request method. Here you can see response data format and samples for each available method.
+
+- Successful `GET` requests will respond with a `200 OK` and in the following format:
+
+```json
+{
+  "data": [
+    {
+      "productCategory": {
+        "S": "Clothing"
+      },
+      "productPrice": {
+        "N": "30"
+      },
+      "productName": {
+        "S": "Shorts"
+      }
+    },
+    {
+      "productCategory": {
+        "S": "Clothing"
+      },
+      "productPrice": {
+        "N": "50"
+      },
+      "productName": {
+        "S": "Shirt"
+      }
+    }
+  ]
+}
+```
+
+- Successful `POST` requests will respond with a `201 Created` and include the new product's ID in the response body and location in the response header.
+
+```json
+{
+  "data": {
+    "productId": 1
+  }
+}
+```
+
+- Successful `DELETE` and `PATCH` requests will respond with a `204 No Content`, which means there will be no response body.
+
+## Error Responses
+
+In the event of an error, the API returns with a `5xx` or `4xx` depending on the error and the error message in the following format:
+
+```json
+{
+  "message": "Error message."
+}
+```
+
+AWS returns errors this way so I formatted DynamoDB exceptions this way as well to be consistent.
---
+++ lambda.js
@@ -0,0 +1,264 @@
+/**
+ * A small AWS Demo application for my Commerce Architects interview
+ * @author Red Oral
+ */
+import {
+  DynamoDBClient,
+  ScanCommand,
+  GetItemCommand,
+  PutItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+  QueryCommand
+} from '@aws-sdk/client-dynamodb';
+
+/* Resource Paths */
+const productsPath = '/products';
+const productIdPath = '/products/{productId}';
+const categoriesPath = '/products/categories';
+const categoiesNamePath = '/products/categories/{categoryName}';
+
+/* Event handler */
+export const handler = async (event) => {
+  const eventBody = JSON.parse(event.body);
+
+  switch (true) {
+    case event.resource === productsPath && event.httpMethod === 'GET':
+      return await getAllProducts();
+    case event.resource === productsPath && event.httpMethod === 'POST':
+      return await createProduct(eventBody);
+    case event.resource === productIdPath && event.httpMethod === 'GET':
+      return await getProductById(event.pathParameters.productId);
+    case event.resource === productIdPath && event.httpMethod === 'PATCH':
+      return await updateProductById(
+        event.pathParameters.productId,
+        eventBody.updateKey,
+        eventBody.updateValue
+      );
+    case event.resource === productIdPath && event.httpMethod === 'DELETE':
+      return await deleteProductById(event.pathParameters.productId);
+    case event.resource === categoiesNamePath && event.httpMethod === 'GET':
+      return await getAllCategories();
+    case event.resource === categoiesNamePath && event.httpMethod === 'GET':
+      return await getProductsByCategory(event.pathParameters.categoryName);
+    default:
+      return errorResponseBuilder(404, 'Resource not found.');
+  }
+};
+
+/* Initialize DynamoDB client */
+const dbClient = new DynamoDBClient();
+
+/**
+ * getAllProducts - Gets all entries in the products table
+ * @returns A response object with a 200 status code and an array of all of the products or an error message
+ */
+const getAllProducts = async () => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    ProjectionExpression: 'productName, productCategory, productPrice'
+  };
+
+  try {
+    const data = await dbClient.send(new ScanCommand(params));
+    return responseBuilder(200, data.Items, productsPath);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * createProduct - Inserts a new product into the product table
+ * @param product - The product object to be inserted
+ *
+ * @returns - A response object with a 201 status code and location header or an error message
+ */
+const createProduct = async (product) => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Item: {
+      productId: { N: product.productId },
+      productName: { S: product.productName },
+      productCategory: { S: product.productCategory },
+      productPrice: { N: product.productPrice }
+    }
+  };
+
+  try {
+    await dbClient.send(new PutItemCommand(params));
+    return responseBuilder(
+      201,
+      { productId: product.productId },
+      `${productsPath}/${product.productId}`
+    );
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ *  getProductById - Gets a single product based on the ID from the products table
+ *  @param id - The productId used to find the item
+ *
+ *  @returns A response object with a 200 status code and the product data or an error message
+ */
+const getProductById = async (id) => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Key: {
+      productId: { N: id }
+    },
+    ProjectionExpression: 'productName, productCategory, productPrice'
+  };
+
+  try {
+    const data = await dbClient.send(new GetItemCommand(params));
+    return responseBuilder(200, data.Item, `${productsPath}/${id}`);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * updateProductById - Updates the products name and/or category based on the given request body
+ * @param id - productId of the product to update
+ * @param updateKey - The key to update in the table
+ * @param updateValue - The value to update in the table
+ *
+ * @returns - A response object with a 204 status code or an error message
+ */
+const updateProductById = async (id, updateKey, updateValue) => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Key: {
+      productId: { N: id }
+    },
+    UpdateExpression: `set ${updateKey} = :v`,
+    ExpressionAttributeValues: {
+      ':v': updateValue
+    }
+  };
+
+  try {
+    await dbClient.send(new UpdateItemCommand(params));
+    return responseBuilder(204, null, `${productsPath}/${id}`);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * deleteProductById - Deletes a product from the products table based on the given ID
+ * @param id - productId of the product to delete
+ *
+ * @returns - A response object with a 204 status code or an error message
+ */
+const deleteProductById = async (id) => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    Key: {
+      productId: { N: id }
+    }
+  };
+
+  try {
+    await dbClient.send(new DeleteItemCommand(params));
+    return responseBuilder(204, null, `${productsPath}/${id}`);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * getProductsByCategory - Gets all products under the same category name
+ * @param category - The category to filter out products with
+ *
+ * @returns - A response object with 200 status code and array of products or an error message
+ */
+const getProductsByCategory = async (category) => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    IndexName: 'productCategory-index',
+    KeyConditionExpression: 'productCategory = :c',
+    ExpressionAttributeValues: {
+      ':c': { S: category }
+    }
+  };
+
+  try {
+    const data = await dbClient.send(new QueryCommand(params));
+    return responseBuilder(200, data.Items, `${categoriesPath}/${category}`);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * getAllCategories - Gets all available categories
+ * @returns - A response object with a 200 status code with an array of categories or an error message
+ */
+const getAllCategories = async () => {
+  const params = {
+    TableName: process.env.PRODUCTS_TABLE,
+    IndexName: 'productCategory-index',
+    ProjectionExpression: 'productCategory'
+  };
+
+  try {
+    const data = await dbClient.send(new ScanCommand(params));
+    const categoryArray = [];
+
+    data.Items.forEach((item) => {
+      categoryArray.push({ S: item.productCategory.S });
+    });
+    return responseBuilder(200, categoryArray, categoriesPath);
+  } catch (e) {
+    return errorResponseBuilder(500, e.message);
+  }
+};
+
+/**
+ * responseBuilder - Function to build HTTP response object
+ * @param status - HTTP status code of the response
+ * @param data - data from the db
+ * @param location - Location of the resource, mainly important for POST
+ *
+ * @returns A response object
+ */
+const responseBuilder = async (status, data, location) => {
+  return {
+    statusCode: status,
+    headers: {
+      Location: location,
+      'Access-Control-Allow-Headers':
+        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT,DELETE'
+    },
+    body: JSON.stringify({
+      data: data
+    })
+  };
+};
+
+/**
+ * errorResponseBuilder - Function to build HTTP response object on exceptions
+ * @param status - HTTP status code of the response
+ * @param message - The exception error message
+ *
+ * @returns A response object
+ */
+const errorResponseBuilder = async (status, message) => {
+  return {
+    statusCode: status,
+    headers: {
+      'Access-Control-Allow-Headers':
+        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT,DELETE'
+    },
+    body: JSON.stringify({
+      message: message
+    })
+  };
+};
